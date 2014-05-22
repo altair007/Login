0a36af9a5fc777f308d139b3e0179af114c22a67
颜风 <1178328901@qq.com>
2014-05-22 19:14:11 +0800

    Initial Commit

---
+++ Login/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Login
+//
+//  Created by   颜风 on 14-5-22.
+//  Copyright (c) 2014年 Shadow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CFAppDelegate class]));
+    }
+}
